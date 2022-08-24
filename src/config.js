a7e75f8e6712ecae13355cc453e644cadf02159c
Jay <jay.bowen@uky.edu>
2022-08-23 19:43:25 -0500

    update config.js

--- src/config.js
+++ src/config.js
@@ -30,8 +30,50 @@ var config = {
             mapAnimation: 'flyTo',
             rotateAnimation: false,
             callback: '',
-            onChapterEnter: [],
-            onChapterExit: []
+            onChapterEnter: [
+              {
+                  layer: 'vaccination_rates',
+                  opacity: 1
+              },
+              {
+                  layer: 'idle_production_capacity',
+                  opacity: 0
+              },
+              {
+                  layer: 'support_trips_waiver',
+                  opacity: 0
+              },
+              {
+                  layer: 'who_instruction',
+                  opacity: 0
+              },
+              {
+                layer: 'who_spokes',
+                opacity: 0
+              }
+            ],
+            onChapterExit: [
+              {
+                  layer: 'vaccination_rates',
+                  opacity: 1
+              },
+              {
+                  layer: 'idle_production_capacity',
+                  opacity: 0
+              },
+              {
+                  layer: 'support_trips_waiver',
+                  opacity: 0
+              },
+              {
+                  layer: 'who_instruction',
+                  opacity: 0
+              },
+              {
+                layer: 'who_spokes',
+                opacity: 0
+              }
+            ]
         },
         {
             id: 'vaxrates',
@@ -55,8 +97,50 @@ var config = {
             mapAnimation: 'flyTo',
             rotateAnimation: false,
             callback:'',
-            onChapterEnter: [],
-            onChapterExit: []
+            onChapterEnter: [
+              {
+                  layer: 'vaccination_rates',
+                  opacity: 1
+              },
+              {
+                  layer: 'idle_production_capacity',
+                  opacity: 0
+              },
+              {
+                  layer: 'support_trips_waiver',
+                  opacity: 0
+              },
+              {
+                  layer: 'who_instruction',
+                  opacity: 0
+              },
+              {
+                layer: 'who_spokes',
+                opacity: 0
+              }
+            ],
+            onChapterExit: [
+              {
+                  layer: 'vaccination_rates',
+                  opacity: 1
+              },
+              {
+                  layer: 'idle_production_capacity',
+                  opacity: 0
+              },
+              {
+                  layer: 'support_trips_waiver',
+                  opacity: 0
+              },
+              {
+                  layer: 'who_instruction',
+                  opacity: 0
+              },
+              {
+                layer: 'who_spokes',
+                opacity: 0
+              }
+            ]
         },
         {
             id: 'reasons',
