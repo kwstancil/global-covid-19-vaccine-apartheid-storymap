74b234fcf81a749a41d21b425ee8521922a53b8d
Kenny Stancil <kenny.stancil@uky.edu>
2022-08-24 20:15:58 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -332,16 +332,7 @@ var config = {
                   opacity: 1
               }
             ],
-            onChapterExit: [
-              {
-                  layer: 'who_spokes',
-                  opacity: 0
-              },
-              {
-                  layer: 'who_instruction',
-                  opacity: 0
-              }
-            ]
+            onChapterExit: []
         },
         {
             id: 'technih',
